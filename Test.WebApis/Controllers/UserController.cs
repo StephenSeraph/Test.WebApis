c536481fe97f2f6beaee1dd491ff25edc7af98cf
testname <test@test.com>
2017-01-03 15:16:19 +0800

    填写基本的CRUD操作

--- Test.WebApis/Controllers/UserController.cs
+++ Test.WebApis/Controllers/UserController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using Test.WebApis.Models;
 
@@ -10,7 +8,70 @@ namespace Test.WebApis.Controllers
 {
     public class UserController : ApiController
     {
-        private User users; 
+        private User users;
+
+        public UserController()
+        {
+            users = new Models.User();
+        }
+
+        public IEnumerable<UserInfo> GetUser()
+        {
+            return users.Users;
+        }
+
+        public IHttpActionResult PostUser([FromBody]UserInfo value)
+        {
+            var guid = Guid.Parse("00000000000000000000000000000000");
+            if (value.Id != guid)
+            {
+                if (users.Users.FirstOrDefault(p => p.Id == value.Id) == null)
+                {
+                    users.Users.Add(value);
+                    users.SaveChanges();
+                    return Ok();
+                }
+            }
+            return BadRequest();
+        }
+
+        public IHttpActionResult PutUser([FromBody]UserInfo value)
+        {
+            var guid = Guid.Parse("00000000000000000000000000000000");
+            if (value.Id != guid)
+            {
+                var user = users.Users.FirstOrDefault(p => p.Id == value.Id);
+                if (user == null)
+                {
+                    users.Users.Add(value);
+                }
+                else
+                {
+                    user.IDCard = value.IDCard;
+                    user.Name = value.Name;
+                    user.Phone = value.Phone;
+                }
+                users.SaveChanges();
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        public IHttpActionResult DeleteUser([FromBody]UserInfo value)
+        {
+            var user = users.Users.FirstOrDefault(p => p.Id == value.Id);
+            if (user != null)
+            {
+                users.Users.Remove(user);
+                users.SaveChanges();
+            }
+            return Ok();
+        }
+
+        /// <summary>
+        /// //////////////////////////////////////////////////////
+        /// </summary>
+        /// <returns></returns>
         // GET: api/User
         public IEnumerable<string> Get()
         {
@@ -20,7 +81,12 @@ namespace Test.WebApis.Controllers
         // GET: api/User/5
         public string Get(int id)
         {
-            return "value";
+            var i = users.Users.Count();
+            var user = users.Users.FirstOrDefault(p => p.IDCard == "3194181827");
+            if (!(user == null))
+                return user.Name;
+            else
+                return "asd";
         }
 
         // POST: api/User
--- Test.WebApis/Models/User.cs
+++ Test.WebApis/Models/User.cs
@@ -10,14 +10,18 @@
         //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
         //使用“User”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
         //“Test.WebApis.Models.User”数据库。
-        // 
+        //
         //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“User”
         //连接字符串。
         public User()
             : base("name=User")
         {
-            var userInfo = new UserInfo { Id = new Guid(), Gender = 0, IDCard = "3194181827", Name = "Tom", Phone = "13611112222" };
-            Users.Add(userInfo);
+            if (Users.Count() == 0)
+            {
+                var userInfo = new UserInfo { Id = Guid.NewGuid(), Gender = 0, IDCard = "3194181827", Name = "Tom", Phone = "13611112222" };
+                Users.Add(userInfo);
+                SaveChanges();
+            }
         }
 
         //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
@@ -36,6 +40,7 @@
         public int Gender { get; set; }
         public string Phone { get; set; }
     }
+
     //public class MyEntity
     //{
     //    public int Id { get; set; }
